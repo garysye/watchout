28c19a08f64328bdbc734938ab20c1667e2ba524
garysye <garysye@gmail.com>
2015-08-28 16:13:27 -0700

    Implemented collision detection and scoreboard ticking

--- client/watchout.js
+++ client/watchout.js
@@ -4,7 +4,8 @@
 var boardOptions = {
   height: 500,
   width: 700,
-  numEnemies: 10
+  numEnemies: 10,
+  turnTime: 1000
 }
 
 var enemyOptions = {
@@ -46,3 +47,70 @@ var playerNode = d3.select("svg").append("circle")
   .attr("cx", player.x )
   .attr("r", player.radius)
   .attr("fill", player.color);
+
+
+//move enemies
+var enemyMove = function() {
+  enemyNodes.transition().duration(boardOptions.turnTime)
+    .attr("cy", function(d) {return Math.random() * boardOptions.height})
+    .attr("cx", function(d) {return Math.random() * boardOptions.width})
+}
+
+var gameTurn = function() {
+  enemyMove();
+}
+
+setInterval(gameTurn, boardOptions.turnTime);
+
+
+//collision detection
+
+var collided = false;
+
+var collisionDetection = function() {
+  //store player variables
+  if (!collided) {
+    var pX = playerNode.attr("cx");
+    var pY = playerNode.attr("cy");
+    var pR = playerNode.attr("r"); 
+
+    //iterate through every enemy
+    enemyNodes[0].forEach(function(enemyDOM) {
+      enemy = d3.select(enemyDOM);
+      var eX = enemy.attr("cx");
+      var eY = enemy.attr("cy");
+      var eR = enemy.attr("r");
+      if (Math.sqrt(Math.pow(pX-eX,2)+Math.pow(pY-eY,2)) < (+pR)+(+eR)) {
+        collided = true;
+        console.log("crash");
+        updateCollisions();
+        updateHighScore();
+        updateScore(0);
+        //gives player small invulnerability before trying again
+        setTimeout(function() {
+          collided = false;
+        }, boardOptions.turnTime);
+      }
+    });
+  }
+}
+
+var updateCollisions = function() {
+  var collisionCount = d3.select('.collisions').select('span').text();
+  d3.select('.collisions').select('span').text(+collisionCount+1);
+}
+setInterval(collisionDetection, 10);
+
+var updateScore = function(score) {
+  score = score !== undefined ? score : d3.select('.current').select('span').text();
+  d3.select('.current').select('span').text(+score+1);
+}
+
+var updateHighScore = function() {
+  var highScore = +(d3.select('.high').select('span').text());
+  var score = +(d3.select('.current').select('span').text());
+  if (highScore < score) {
+    d3.select('.high').select('span').text(score);
+  }
+}
+setInterval(updateScore, 100);
